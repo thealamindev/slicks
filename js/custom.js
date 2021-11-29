f75610caa8d32dd5b44f262b777a5a8ea2cac43f
Al-Amin <37735466+thealamindev@users.noreply.github.com>
2021-11-29 15:31:23 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+// Slick Sliders 
+$('.sliders').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:true,
+    prevArrow: ' <i class="far fa-arrow-alt-circle-left prev"></i>',
+    nextArrow: '<i class="far fa-arrow-alt-circle-right next"></i>',
+  });
+});
